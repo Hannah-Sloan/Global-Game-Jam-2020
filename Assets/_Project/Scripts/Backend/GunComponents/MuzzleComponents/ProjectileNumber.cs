e4a0cbf5951533c224333c078cfeedccf111762e
Riley Baker <vishnukey@gmail.com>
2020-02-01 04:08:22 -0700

    Added all gun components as separate monodbehaviours

--- Assets/_Project/Scripts/Backend/GunComponents/MuzzleComponents/ProjectileNumber.cs
+++ Assets/_Project/Scripts/Backend/GunComponents/MuzzleComponents/ProjectileNumber.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunComponent : MonoBehaviour
+public class ProjectileNumber : MuzzleComponent
 {
+    public int value;
     // Start is called before the first frame update
     void Start()
     {
