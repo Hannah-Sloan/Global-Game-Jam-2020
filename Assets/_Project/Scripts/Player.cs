9635cb2c8ef470d46b0e5011afd04f7a224d8d46
Riley Baker <vishnukey@gmail.com>
2020-02-01 02:20:52 -0700

    Made a skeleton of the backend
    Created all the necessary files for the Gun and all of it's components
    Made a skelton for the components themsevles
    Made a player class and gave it a health property

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
