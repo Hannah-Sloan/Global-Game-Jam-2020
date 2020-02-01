ca3c91f464bcb2d262e7c077cedf2dcabbb88109
Hannah-Sloan <hannah.ava.sloan@gmail.com>
2020-02-01 15:24:15 -0700

    Gun breakage

---
+++ Assets/BreakIt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakIt : Singleton<BreakIt>
+{
+    public void OnBreak(System.Type componentTypeToBreak)
+    {
+        if (componentTypeToBreak != typeof(GunComponent))
+        {
+            Debug.LogError("ERROR ERROR".Bold().Size(35));
+            Debug.LogError("RILEY!!!".Bold().Size(35));
+            Debug.LogError(("THAT IS" + "NOT".Bold() + "A GUN COMPONENT").Size(35));
+        }
+    }
+}
