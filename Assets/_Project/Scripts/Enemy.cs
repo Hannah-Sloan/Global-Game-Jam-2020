0bad0a368a0828c955a87ec3d041f9d7dcd50beb
Riley Baker <vishnukey@gmail.com>
2020-02-02 16:48:41 -0700

    new scenes

--- Assets/_Project/Scripts/Enemy.cs
+++ Assets/_Project/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int maxHealth;
