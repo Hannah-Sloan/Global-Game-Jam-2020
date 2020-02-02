18bc8647f2a6160cc47a70ce5d14fde653176a29
Hannah-Sloan <hannah.ava.sloan@gmail.com>
2020-02-02 06:37:49 -0700

    Doesn't destroy the universe in main scene

--- Assets/_Project/Scripts/UI/Managers/BreakIt.cs
+++ Assets/_Project/Scripts/UI/Managers/BreakIt.cs
@@ -12,15 +12,26 @@ public class BreakIt : Singleton<BreakIt>
     public Transform Barrel;
     public Transform Body;
 
+    public bool ImGonnaWreckIt = false;
+
+    private void Start()
+    {
+        UIEnableDisable.Instance.UIOn += WreckIt;
+    }
+
+    System.Type componentTypeToBreak;
+    System.Action done;
 
     public void OnBreak(System.Type componentTypeToBreak, System.Action done)
     {
-        if (componentTypeToBreak != typeof(GunComponent))
-        {
-            Debug.LogError("ERROR ERROR".Bold().Size(35));
-            Debug.LogError("RILEY!!!".Bold().Size(35));
-            Debug.LogError(("THAT IS" + "NOT".Bold() + "A GUN COMPONENT").Size(35));
-        }
+        this.componentTypeToBreak = componentTypeToBreak;
+        this.done = done;
+        ImGonnaWreckIt = true;
+    }
+
+    public void WreckIt()
+    {
+        if (ImGonnaWreckIt) ImGonnaWreckIt = false;
 
         Transform breakVisPos;
         //Check if barrel
