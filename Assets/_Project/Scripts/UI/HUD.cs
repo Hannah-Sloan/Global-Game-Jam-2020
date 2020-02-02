4e328d7e607f50c5f4e5e325fe07697052cf33c5
Riley Baker <vishnukey@gmail.com>
2020-02-01 22:30:28 -0700

    Added ammo counter in Main scene

---
+++ Assets/_Project/Scripts/UI/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI ammoText;
+
+    public void OnAmmoChange(int ammo)
+    {
+        ammoText.text = $"ammo: {ammo}";
+    }
+
+    public void OnReload(bool reloading)
+    {
+        if (reloading)
+        {
+            ammoText.text = $"ammo: reloading!";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Trigger.Instance.OnAmmoChange += OnAmmoChange;
+        Trigger.Instance.OnReload += OnReload;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
